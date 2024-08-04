73b8b9a955b550e3372909b5f915432ff4dc90ae
Celine86 <celine.obringer@live.fr>
2024-08-04 22:48:46 +0200

    New test

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -10,7 +10,7 @@ export function Header() {
         <div id='modal'></div>
         <header>
             <div className='header__elmt'>
-                <a href='/'>
+                <a href='/#/'>
                     <img className='website' src={catPawGreen}
                             onMouseOver={e => e.currentTarget.src = catPawYellow}
                             onMouseOut={e => e.currentTarget.src = catPawGreen}
