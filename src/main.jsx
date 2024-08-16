dfa0410ecd0bb0cdbd65b6ffd3fe9378facc64bb
Celine86 <celine.obringer@live.fr>
2024-08-16 15:17:40 +0200

    add serviceworker

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx'
+import App from './App.jsx';
+import { registerSW } from "virtual:pwa-register";
+
+if ("serviceWorker" in navigator) {
+  // && !/localhost/.test(window.location)) {
+  registerSW();
+}
 
 const container = document.getElementById('root');
 const root = createRoot(container);
